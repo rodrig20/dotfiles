cec7e17e864113ea31f61927e9243fbbb6f86d10
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2025-07-28 21:52:38 +0100

    Change language

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 SUBDIR="$SCRIPT_DIR/aux"
 
-# Executa os scripts um a um
+# Run Scripts
 bash "$SUBDIR/install_packages.sh"
 bash "$SUBDIR/create_sym.sh"
 bash "$SUBDIR/sddm.sh"
