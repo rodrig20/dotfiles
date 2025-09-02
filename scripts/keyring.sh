4ec8efcb2ea995f4c128ee9c8b806dd23f2d00f9
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2025-09-02 20:43:36 +0100

    Fix keyring

--- scripts/keyring.sh
+++ scripts/keyring.sh
@@ -4,4 +4,4 @@ sudo pacman -S --needed gnome-keyring
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cp "$SCRIPT_DIR/aux/files/" "/etc/pam.d/sddm"
+sudo cp "$SCRIPT_DIR/aux/files/sddm" "/etc/pam.d/sddm"
